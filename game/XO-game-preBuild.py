dd28a4f0741139246c98830f96574c764a44a64a
mbpye <73852907+mbpye@users.noreply.github.com>
2024-10-11 00:14:35 +0300

    tick-tack

--- game/XO-game-preBuild.py
+++ game/XO-game-preBuild.py
@@ -1,88 +1,144 @@
-# GAME Base for RL agent (testing new funcs etc.)
-
+import pygame
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QGridLayout, QLabel
-from PyQt5.QtCore import Qt
+from math import inf as infinity
+from random import choice
+import numpy as np
 
-L = 3 #rows
-R = 3 #cols
+pygame.init()
 
+# Константы
+SCREEN_SIZE = (300, 300)
+LINE_COLOR = (23, 145, 135)
+BG_COLOR = (28, 170, 156)
+LINE_WIDTH = 15
+BOARD_ROWS = 3
+BOARD_COLS = 3
+CIRCLE_RADIUS = 60
+CIRCLE_WIDTH = 15
+CROSS_WIDTH = 25
+SPACE = 55
+CIRCLE_COLOR = (239, 231, 200)
+CROSS_COLOR = (66, 66, 66)
 
-class TicTacToe(QWidget):
-    def __init__(self):
-        super().__init__()
+# Глобальные переменные
+board = np.zeros((BOARD_ROWS, BOARD_COLS))
+player = 1  # 1 - Играет человек, -1 - играет компьютер
 
-        self.initUI()
+# Окно игры
+screen = pygame.display.set_mode(SCREEN_SIZE)
+pygame.display.set_caption('Крестики-нолики')
+screen.fill(BG_COLOR)
 
-    def initUI(self):
-        self.board = [[' ' for _ in range(L)] for _ in range(R)]
-        self.current_player = "X"
-        self.moves = 0
+def draw_lines():
+    # Горизонтальные линии
+    pygame.draw.line(screen, LINE_COLOR, (0, 100), (300, 100), LINE_WIDTH)
+    pygame.draw.line(screen, LINE_COLOR, (0, 200), (300, 200), LINE_WIDTH)
+    # Вертикальные линии
+    pygame.draw.line(screen, LINE_COLOR, (100, 0), (100, 300), LINE_WIDTH)
+    pygame.draw.line(screen, LINE_COLOR, (200, 0), (200, 300), LINE_WIDTH)
 
-        self.layout = QGridLayout()
+def draw_figures():
+    for row in range(BOARD_ROWS):
+        for col in range(BOARD_COLS):
+            if board[row][col] == 1:
+                pygame.draw.circle(screen, CIRCLE_COLOR, (int(col * 100 + 100 / 2), int(row * 100 + 100 / 2)), CIRCLE_RADIUS, CIRCLE_WIDTH)
+            elif board[row][col] == -1:
+                pygame.draw.line(screen, CROSS_COLOR, (col * 100 + SPACE, row * 100 + 100 - SPACE), (col * 100 + 100 - SPACE, row * 100 + SPACE), CROSS_WIDTH)
+                pygame.draw.line(screen, CROSS_COLOR, (col * 100 + SPACE, row * 100 + SPACE), (col * 100 + 100 - SPACE, row * 100 + 100 - SPACE), CROSS_WIDTH)
 
-        for i in range(L):
-            for j in range(R):
-                button = QPushButton('')
-                button.clicked.connect(lambda ch, r=i, c=j: self.make_move(r, c))
-                self.layout.addWidget(button, i, j)
-
-        self.setLayout(self.layout)
-
-        self.setWindowTitle('Tic Tac Toe')
-        self.show()
-
-    def make_move(self, row, col):
-        if self.board[row][col] == ' ':
-            self.board[row][col] = self.current_player
-            self.display_board()
-            self.moves += 1
-            if self.check_win():
-                self.game_over(f"Player {self.current_player} wins!")
-                self.disable_buttons()
-            elif self.moves == L*R:
-                self.game_over("It's a tie!")
-                self.disable_buttons()
-            else:
-                self.current_player = "O" if self.current_player == "X" else "X"
-
-    def display_board(self):
-        for i in range(L):
-            for j in range(R):
-                button = self.layout.itemAtPosition(i, j).widget()
-                button.setText(self.board[i][j])
-
-    def check_win(self):
-        for row in self.board:
-            if row[0] == row[1] == row[2] != ' ':
-                return True
-
-        for col in range(L):
-            if self.board[0][col] == self.board[1][col] == self.board[2][col] != ' ':
-                return True
-
-        if self.board[0][0] == self.board[1][1] == self.board[2][2] != ' ':
+def check_win(player):
+    for col in range(BOARD_COLS):
+        if board[:, col].sum() == 3 * player:
+            
             return True
-
-        if self.board[0][2] == self.board[1][1] == self.board[2][0] != ' ':
+    for row in range(BOARD_ROWS):
+        if board[row, :].sum() == 3 * player:
+            
             return True
-
-        return False
-
-    def game_over(self, message):
-        winner_label = QLabel(message)
-        winner_label.setAlignment(Qt.AlignCenter)
+    if board.trace() == 3 * player:
         
-        self.layout.addWidget(winner_label, L, 0, 1, R)
-        self.setLayout(self.layout)
+        return True
+    if np.fliplr(board).trace() == 3 * player:
+        return True
+    return False
 
-    def disable_buttons(self):
-        for i in range(L):
-            for j in range(R):
-                button = self.layout.itemAtPosition(i, j).widget()
-                button.setEnabled(False)
+def check_draw():
+    return np.all(board != 0)
 
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    ex = TicTacToe()
-    sys.exit(app.exec_())
+def empty_cells():
+    return [(row, col) for row in range(BOARD_ROWS) for col in range(BOARD_COLS) if board[row][col] == 0]
+
+def minimax(state, depth, player):
+    if player == 1:
+        best = [-1, -1, -infinity]
+    else:
+        best = [-1, -1, +infinity]
+
+    if depth == 0 or check_win(-1) or check_win(1):
+        score = evaluate(state)
+        return [-1, -1, score]
+
+    for cell in empty_cells():
+        x, y = cell[0], cell[1]
+        state[x][y] = player
+        score = minimax(state, depth - 1, -player)
+        state[x][y] = 0
+        score[0], score[1] = x, y
+
+        if player == 1:
+            if score[2] > best[2]:
+                best = score  # max value
+        else:
+            if score[2] < best[2]:
+                best = score  # min value
+
+    return best
+
+def evaluate(state):
+    if check_win(1):
+        score = +1
+    elif check_win(-1):
+        score = -1
+    else:
+        score = 0
+    return score
+
+def ai_turn():
+    depth = len(empty_cells())
+    if depth == 0 or check_win(1) or check_win(-1):
+        return
+
+    move = minimax(board, depth, -1)
+    row, col = move[0], move[1]
+    board[row][col] = -1
+
+def player_move(row, col):
+    board[row][col] = 1
+
+draw_lines()
+
+# Основной цикл игры
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.MOUSEBUTTONDOWN and not check_win(player) and not check_win(-player):
+            mouseX = event.pos[0]  # x
+            mouseY = event.pos[1]  # y
+            clicked_row = int(mouseY // 100)
+            clicked_col = int(mouseX // 100)
+
+            if board[clicked_row][clicked_col] == 0:
+                player_move(clicked_row, clicked_col)
+                if check_draw():
+                    running = False
+                    print('draw')
+                ai_turn()
+        draw_figures()
+        pygame.display.update()
+
+    if check_win(1) or check_win(-1) or check_draw():
+        running = False
+
+#pygame.quit()
